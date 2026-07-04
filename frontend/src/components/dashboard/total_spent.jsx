c82b83beea12d95d806967c62f67c1a496a2b1c1
megryanrojo <mdrojo.chmsu@gmail.com>
2026-07-05 00:29:05 +0800

    refactor: fixed white screen of death dumb mistake

--- frontend/src/components/dashboard/total_spent.jsx
+++ frontend/src/components/dashboard/total_spent.jsx
@@ -1,13 +1,11 @@
-import { useState, useEffect } from 'react';
-
-const [totalSpent, setTotalSpent] = useState(0);
-
+import { useState } from 'react';
+/*
 useEffect(() => {
     getExpenses();
 }, []);
 
 
-async function getExpenses() {
+    async function getExpenses() {
     const response = await fetch("http://127.0.0.1:8000/api/v1/expenses",
 
         {
@@ -18,13 +16,17 @@ async function getExpenses() {
         }
     )
 }
-
+*/
 function TotalSpent() {
+    const [totalSpent] = useState(0);
+
     return (
-        <div className="bg-zinc-800 flex items-left flex-col gap-2 p-5 border border-zinc-500">
-            <h1 className='text-zinc-400'>Total Spent</h1>
-            <p className='text-zinc-100'>test</p>
-        </div>
+        <>
+            <div className="bg-zinc-800 flex items-left flex-col gap-2 p-5 border border-zinc-500">
+                <h1 className='text-zinc-400'>Total Spent</h1>
+                <p className='text-zinc-100'>{totalSpent}</p>
+            </div>
+        </>
     )
 }
 export default TotalSpent; 
