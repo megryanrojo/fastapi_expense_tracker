a8d5ac0a2f441643b51ea5ee01f4b6aeb11e78c1
megryanrojo <mdrojo.chmsu@gmail.com>
2026-07-20 17:26:33 +0800

    feat: initial implementation of transactions page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, useNavigate } from 'react-router-dom'
 import './App.css'
 import Dashboard from './pages/dashboard'
 import LandingPage from './pages/landingpage'
+import TransactionPage from './pages/transaction'
 
 function App() {
   const navigate = useNavigate();
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<LandingPage/>}></Route>
         <Route path='/dashboard' element={<Dashboard/>}></Route>
+        <Route path='/transactions' element={<TransactionPage/>}></Route>
       </Routes>
     </>
   )
---
+++ frontend/src/pages/transaction.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import NavBar from "../components/navbar";
+import SideBar from "../components/side_bar";
+
+const TransactionPage = () => {
+    const navigate = useNavigate();
+
+    const [user, setUser] = useState(null);
+    const [sidebarOpen, setSidebarOpen] = useState(false);
+
+    useEffect(() => {
+        const userData = JSON.parse(localStorage.getItem("user"));
+        if (userData !== null) {
+            setUser(userData);
+        } else {
+            navigate("/");
+        }
+    }, []);
+
+    function handleLogout() {
+        setUser(null);
+        localStorage.removeItem("user");
+        navigate("/");
+    }
+    
+    return (
+        <div className="min-h-screen bg-zinc-950 text-white">
+            <NavBar user={user} handleLogout={handleLogout} handleSidebarToggle={() => setSidebarOpen(!sidebarOpen)} />
+            <SideBar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
+            <div className="max-w-7xl mx-auto px-6 md:px-10 py-10">
+
+                {/* Add your transaction content here */}
+            </div>
+        </div>
+    )
+
+}
+export default TransactionPage;
